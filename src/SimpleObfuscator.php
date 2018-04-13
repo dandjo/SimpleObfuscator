ad8ac2c6a594c3bb2e65b4543ea8e2859a8e1514
Daniel Pernold <daniel.pernold@gmail.com>
2018-04-13 16:53:39 +0200

    Initial commit

---
+++ src/SimpleObfuscator.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace SimpleObfuscator;
+
+/**
+ * Class SimpleObfuscator.
+ * Encryps and decrypts strings with given salt. Uses hexadecimal representation
+ * of the calculated binary string.
+ */
+class SimpleObfuscator {
+
+    /**
+     * @var string
+     */
+    private $salt;
+
+    /**
+     * SimpleObfuscator constructor.
+     * @param $salt
+     */
+    public function __construct($salt) {
+        $this->salt = $salt;
+    }
+
+    /**
+     * @param $string
+     * @return string
+     */
+    function decrypt($string) {
+        $result = '';
+        foreach (str_split(hex2bin($string)) as $i => $char) {
+            $saltChar = substr($this->salt, ($i % strlen($this->salt)) - 1, 1);
+            $result .= chr(ord($char) - ord($saltChar));
+        }
+        return $result;
+    }
+
+    /**
+     * @param $string
+     * @return string
+     */
+    function encrypt($string) {
+        $result = '';
+        foreach (str_split($string) as $i => $char) {
+            $saltChar = substr($this->salt, ($i % strlen($this->salt)) - 1, 1);
+            $result .= chr(ord($char) + ord($saltChar));
+        }
+        return bin2hex($result);
+    }
+
+}
